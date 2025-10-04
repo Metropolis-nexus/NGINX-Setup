69d5bc146c4b903211d27eef5be264b03b843cd5
Tommy <contact@tommytran.io>
2025-10-03 19:22:23 -0700

    Download http.conf

--- setup.sh
+++ setup.sh
@@ -143,6 +143,7 @@ sudo systemctl enable --now rotate-session-ticket-keys.timer
 # Download NGINX configs
 if [ "${deployment_mode}" = 1 ] || [ "${deployment_mode}" = 3 ]; then
     unpriv curl -s https://raw.githubusercontent.com/Metropolis-Nexus/NGINX-Setup/main/etc/nginx/conf.d/default.conf | sudo tee /etc/nginx/conf.d/default.conf > /dev/null
+    unpriv curl -s https://raw.githubusercontent.com/Metropolis-Nexus/NGINX-Setup/main/etc/nginx/conf.d/http.conf | sudo tee /etc/nginx/conf.d/http.conf > /dev/null
 fi 
 
 if [ "${deployment_mode}" = 1 ]; then
